5d9558828f635604944fa4d275d574be9e8f87d4
eric-zhu <ericzhu77@gmail.com>
2018-03-15 09:59:15 -0700

    added env info

--- scripts/env.py
+++ scripts/env.py
@@ -8,7 +8,7 @@ def set_db_env():
     setting environment variables up
     '''
     os.environ['DB_USER'] = 'root'
-    os.environ['DB_PASSWORD'] = 'spacerock1290'
+    os.environ['DB_PASSWORD'] = '...'
     os.environ['DB_HOST'] = 'localhost'
     os.environ['DB_PORT'] = '3306'
     print 'Environment has been configured'
