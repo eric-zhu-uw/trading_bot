2bb2e1a7edbdaf28e59a63bbe49572b843824f7c
eric-zhu <ericzhu77@gmail.com>
2018-03-18 15:43:22 -0700

    update binance kline fetching

--- scripts/get_klines.py
+++ scripts/get_klines.py
@@ -1,13 +1,12 @@
 '''
-get_klines
+populate mysql database with kline data from Binance
 '''
-
 import datetime
 from binance.client import Client
-from create_klines_schema import *
+from create_klines_schema import Kline1, Kline3, Kline5, Kline15, Kline30
 import constants    #figure out how to clean up
 
-def get_klines(TableName, interval, start_time=constants.START_DATE):
+def get_klines(symbol, kline_interval, start_time=constants.START_DATE):
     '''
     get_klines
     '''
@@ -15,12 +14,32 @@ def get_klines(TableName, interval, start_time=constants.START_DATE):
     if client.ping() == {}:
         print 'Binance Connection Successful!'
 
+    # default parameters
+    limit = 500
+    interval = client.KLINE_INTERVAL_1MINUTE
+    KlineModel = Kline1
+    time_between_start_time = kline_interval * limit * constants.MINUTE
+
+    # update parameters depending on interval
+    if kline_interval == 3:
+        KlineModel = Kline3
+        interval = client.KLINE_INTERVAL_3MINUTE
+    elif kline_interval == 5:
+        KlineModel = Kline5
+        interval = client.KLINE_INTERVAL_5MINUTE
+    elif kline_interval == 15:
+        KlineModel = Kline15
+        interval = client.KLINE_INTERVAL_15MINUTE
+    elif kline_interval == 30:
+        KlineModel = Kline30
+        interval = client.KLINE_INTERVAL_30MINUTE
+
     while start_time < constants.END_DATE:
         try:
             klines = client.get_klines(
-                symbol=constants.ETH_USDT,
+                symbol=symbol,
                 interval=interval,
-                limit=500,
+                limit=limit,
                 startTime=start_time
             )
         except Exception as e:
@@ -34,7 +53,7 @@ def get_klines(TableName, interval, start_time=constants.START_DATE):
 
         for k in klines:
             try:
-                TableName.create(
+                KlineModel.create(
                     open_time=k[0],
                     open=float(k[1]),
                     high=float(k[2]),
@@ -42,7 +61,7 @@ def get_klines(TableName, interval, start_time=constants.START_DATE):
                     close=float(k[4]),
                     volume=float(k[5]),
                     close_time=k[6],
-                    quote_asset_volume=float(k[7]),
+                    total_quote_asset_volume=float(k[7]),
                     num_of_trades=k[8],
                     taker_buy_base_asset_volume=float(k[9]),
                     taker_buy_quote_asset_volume=float(k[10])
@@ -60,12 +79,12 @@ def get_klines(TableName, interval, start_time=constants.START_DATE):
         with open('success.log', 'a') as f:
             f.write('========================================\n')
             f.write('Finished : [' + str(start_time) + ', ' +
-                    str(start_time + (500 * constants.MINUTE)) + ']\n')
+                    str(start_time + time_between_start_time) + ']\n')
             f.write('Passed Time: ' + str(datetime.datetime.now()) + '\n')
             f.close()
 
-        start_time = start_time + (500 * constants.MINUTE)
+        start_time = start_time + time_between_start_time
 
 if __name__ == "__main__":
-    get_klines(KlineOne, Client.KLINE_INTERVAL_1MINUTE)
+    get_klines(constants.ETH_USDT, 1514017800000 + (30 * constants.MINUTE))
     print 'Finished script!'
