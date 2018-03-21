e166b1f84158de67f1d4afa26dfb96a6f5d6ba61
eric-zhu <ericzhu77@gmail.com>
2018-03-21 02:21:27 -0700

    testing info

---
+++ scripts/testing.py
@@ -0,0 +1,7 @@
+
+#finished training and don't want to retrain
+# save the weights are already saved into the <somename.hdf5> so you don't wanna retrain
+# model.load_weights('somename.hdf5')
+# model.evaluate()
+## input vector sequence in same format and get result
+# model.predict()
