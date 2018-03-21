983941736dffb9efe1baf5a5206aef321a34646d
eric-zhu <ericzhu77@gmail.com>
2018-03-21 02:21:42 -0700

    features get_info

--- scripts/features.py
+++ scripts/features.py
@@ -8,9 +8,17 @@ class Features:
     def __init__(self, training_ratio=0.8):
         self.data_frame = pd.read_csv('kline_one.csv')
         self.training_ratio = training_ratio
-        self.training_count = int(int(self.df.describe()['open_time']['count']) * training_ratio)
+        self.count = int(self.data_frame.describe()['open_time']['count'])
+        self.training_count = int(self.count * training_ratio)
 
-    def get_Xtrain(self, dataRange):
+    def get_info(self):
+        return {
+            'training_ratio': self.training_ratio,
+            'count': self.count,
+            'training_count': self.training_count
+        }
+
+    def get_x_train(self, dataRange=0):
         '''
         get the X-training set
         '''
@@ -18,10 +26,10 @@ class Features:
         for i in range(0, 1000):
             x_val = self.data_frame.iloc[i:i+30][['open']].transpose().iloc[0]
             x_train.append(x_val.values)
-            x_train = np.arrray(x_train)
+        x_train = np.array(x_train)
         return x_train
 
-    def get_YTrain(self, dataRange):
+    def get_y_train(self, dataRange=0):
         '''
         get the Y-training set
         '''
@@ -29,5 +37,5 @@ class Features:
         for i in range(0, 1000):
             y_val = self.data_frame.iloc[i+30][['open']]
             y_train.append(y_val.values)
-            y_train = np.array(y_train)
+        y_train = np.array(y_train)
         return y_train
